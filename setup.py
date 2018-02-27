3a1948e2255746593a333b0dd556b5f5a3bd5026
zj-zhang <zzj.zju@gmail.com>
2018-02-27 11:53:55 -0800

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 def main():
 	setup(name='CLAM',
-		  version='1.1.0',
+		  version='1.1.2',
 		  description='CLIP-seq Analysis of Multi-mapped reads',
 		  author='Zijun Zhang',
 		  author_email='zj.z@ucla.edu',
